4f0cc3af15ac16be0a62c6b2e4c4204798f95ad9
Yogesh Datir <yogdat@gmail.com>
2026-03-13 00:13:23 +0530

    feat: shadcn initialization

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,4 +15,9 @@ export default defineConfig({
     tailwindcss(),
     svgr(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
